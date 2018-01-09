bc1c886ef4df011635fc68d106efcd0c975b1928
Camilo Segura <camilosegurar@gmail.com>
2018-01-09 17:47:11 -0500

    feat: Added redux support

---
+++ reducers/decks.js
@@ -0,0 +1,13 @@
+import { ADD_CARD, ADD_DECK } from '../utils/constants';
+
+function entries (state = {}, action) {
+  const { type } = action;
+
+  switch (type) {
+
+    default :
+      return state
+  }
+}
+
+export default entries
